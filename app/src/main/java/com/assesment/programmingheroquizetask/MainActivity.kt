521f03c0c7aa902e35478a9412e0de0f15991465
Shafi <jonyszone@gmail.com>
2023-10-21 07:24:02 +0600

    initial setup with required dependencies

---
+++ app/src/main/java/com/assesment/programmingheroquizetask/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.assesment.programmingheroquizetask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.assesment.programmingheroquizetask.ui.theme.ProgrammingHeroQuizeTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProgrammingHeroQuizeTaskTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProgrammingHeroQuizeTaskTheme {
+        Greeting("Android")
+    }
+}
