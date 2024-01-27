2474d73ef8c4152bbf9ba3c3532a921eb3d5eb5e
Shafi <jonyszone@gmail.com>
2024-01-27 21:20:26 +0600

    primitive type example

--- src/main/kotlin/fundamental/Types.kt
+++ src/main/kotlin/fundamental/Types.kt
@@ -1,15 +1,20 @@
 package fundamental
 
 fun main() {
-    val number: Byte = 127
+    var number: Byte = 127
     val counts: Long = 3_123_123_123  //explicit types is redundant
-    val countsImpl =  3_123_123_123 //similar like counts
+    val countsImpl = 3_123_123_123 //similar like counts
     val price: Float = 10.16F
     val letter: Char = 'A'
     val isOkay: Boolean = true
 
-   if (isOkay)
-        println("Number: $number and Character: $letter")
+    val numberExt = number
+    number = 100 //not updated cause its primitive type
+
+    if (isOkay)
+        println("Number: $numberExt and Character: $letter")
     else
         print("Counts: $counts and Price: $price")
+
+
 }
